2a5ba36db1783cfdfd826899d404873e7694eb9f
Abhilash Saj <abhilashsaj@gmail.com>
2020-03-27 20:19:11 +0530

    style updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css'
 import cartReducer from './components/reducers/cartReducer';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
