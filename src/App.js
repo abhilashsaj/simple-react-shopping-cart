5df01900aa018558f17306f36b6431a6869c0d7c
Abhilash Saj <abhilashsaj@gmail.com>
2020-03-27 20:17:03 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Navbar from './components/Navbar'
-import { BrowserRouter } from "react-router-dom";
-import Home from './components/Home';
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar/>
-      <Home/>
+import Home from './components/Home'
+import Cart from './components/Cart'
 
-    </BrowserRouter>
-    
-  );
+class App extends Component {
+  render() {
+    return (
+       <BrowserRouter>
+            <div className="App">
+            
+              <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/cart" component={Cart}/>
+                  </Switch>
+             </div>
+       </BrowserRouter>
+      
+    );
+  }
 }
 
 export default App;
