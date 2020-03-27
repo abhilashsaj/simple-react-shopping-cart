f9c64637bf338fec6b9e47857dc6b929e7f7b6c1
Abhilash Saj <abhilashsaj@gmail.com>
2020-03-27 20:13:01 +0530

    bug fixes

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,8 +1,12 @@
-
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 //import { addShipping } from './actions/cartActions'
 class Recipe extends Component{
+    
+    componentWillUnmount() {
+         if(this.refs.shipping.checked)
+              this.props.substractShipping()
+    }
 
     handleChecked = (e)=>{
         if(e.target.checked){
@@ -14,7 +18,7 @@ class Recipe extends Component{
     }
 
     render(){
-        
+  
         return(
             <div className="container">
                 <div className="collection">
@@ -33,18 +37,3 @@ class Recipe extends Component{
         )
     }
 }
-const mapStateToProps = (state)=>{
-    return{
-        addedItems: state.addedItems,
-        total: state.total
-    }
-}
-
-const mapDispatchToProps = (dispatch)=>{
-    return{
-        addShipping: ()=>{dispatch({type: 'ADD_SHIPPING'})},
-        substractShipping: ()=>{dispatch({type: 'SUB_SHIPPING'})}
-    }
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(Recipe)
