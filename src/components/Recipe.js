f04c6daf93af0117706b29fbcd43b13e338f42e6
Abhilash Saj <abhilashsaj@gmail.com>
2020-03-27 20:13:48 +0530

    updates

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -37,3 +37,19 @@ class Recipe extends Component{
         )
     }
 }
+
+const mapStateToProps = (state)=>{
+    return{
+        addedItems: state.addedItems,
+        total: state.total
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return{
+        addShipping: ()=>{dispatch({type: 'ADD_SHIPPING'})},
+        substractShipping: ()=>{dispatch({type: 'SUB_SHIPPING'})}
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Recipe)
