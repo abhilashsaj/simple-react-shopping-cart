8a30bd868758e2ba7a3ae3608828441b6e031be9
Abhilash Saj <abhilashsaj@gmail.com>
2020-03-27 19:58:28 +0530

    added actions, cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import { BrowserRouter } from "react-router-dom";
+import Home from './components/Home';
 function App() {
   return (
     <BrowserRouter>
       <Navbar/>
-
+      <Home/>
 
     </BrowserRouter>
     
--- src/components/reducers/cartReducer.js
+++ src/components/reducers/cartReducer.js
@@ -4,6 +4,7 @@ import Item3 from '../../images/item3.jpg'
 import Item4 from '../../images/item4.jpg'
 import Item5 from '../../images/item5.jpg'
 import Item6 from '../../images/item6.jpg'
+import { ADD_TO_CART} from '../actions/action-types/cart-actions'
 
 
 const initState = {
@@ -20,8 +21,35 @@ const initState = {
 
 }
 const cartReducer= (state = initState,action)=>{
-    
-    return state;
-
-}
+   
+    //INSIDE HOME COMPONENT
+    if(action.type === ADD_TO_CART){
+          let addedItem = state.items.find(item=> item.id === action.id)
+          //check if the action id exists in the addedItems
+         let existed_item= state.addedItems.find(item=> action.id === item.id)
+         if(existed_item)
+         {
+            addedItem.quantity += 1 
+             return{
+                ...state,
+                 total: state.total + addedItem.price 
+                  }
+        }
+         else{
+            addedItem.quantity = 1;
+            //calculating the total
+            let newTotal = state.total + addedItem.price 
+            
+            return{
+                ...state,
+                addedItems: [...state.addedItems, addedItem],
+                total : newTotal
+            }
+            
+        }
+    }
+    else{
+        return state
+    }
+  }
 export default cartReducer;
